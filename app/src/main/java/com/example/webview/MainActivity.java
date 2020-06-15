a9fab2bbfc7d3928b8c35dab408404dc5d175107
Lui <Soler>
2020-06-15 20:44:07 -0300

    Primer Web App

---
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    private String url = "https://lmsh.github.io/";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+
+
+        final WebSettings ajustesVisorWeb = webView.getSettings();
+        ((WebSettings) ajustesVisorWeb).setJavaScriptEnabled(true);
+
+        webView.loadUrl(url);
+
+
+    }
+}
